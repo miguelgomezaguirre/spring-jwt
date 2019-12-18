e7aa2d2fd8a2ae13d3731b725317f45cd4ed3d0f
00232603 <miguel.gomez@aa.com>
2019-12-18 12:52:25 -0300

    first commit

--- src/main/java/com/example/demo/controladores/ClienteRestController.java
+++ src/main/java/com/example/demo/controladores/ClienteRestController.java
@@ -5,7 +5,6 @@ import com.example.demo.view.xml.ClienteList;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
